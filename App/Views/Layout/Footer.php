eb372c80d875aa55d0bdee91130eb6e0e805dd83
Qizzy Nazzy Mogno <168419766+Devvrp@users.noreply.github.com>
2025-11-11 20:48:14 +0800

    Delete App directory

--- App/Views/Layout/Footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-        <footer class="content-footer">
-            <p>&copy; 2025 Manajemen Kos. All rights reserved.</p>
-        </footer>
-    </div> 
-    <script src="JS/app.js"></script>
-</body>
-</html>
